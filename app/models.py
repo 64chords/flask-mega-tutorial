dea6a14a6f57b161441f78b9b4ef67ac4f9404a0
statefb <tmgf.15.su@gmail.com>
2018-01-08 18:09:04 +0900

    finish chap10

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 # -*- encoding: utf-8 -*-
 from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
+from time import time
 from hashlib import md5
 from flask_login import UserMixin
-from app import db,login
+import jwt
+from app import db,login,app
 
 """
 current_user変数を参照した際に
@@ -92,6 +94,24 @@ class User(UserMixin,db.Model):
         own = Post.query.filter_by(user_id=self.id)
         #結合し、時系列に並べ替え
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self,expires_in=600):
+        """
+        decode('utf-8')は必須∵jwt.encode()はトークンをバイトでリターンするため、
+        文字列で扱いたい
+        """
+        return jwt.encode(
+            {"reset_password":self.id,"exp":time() + expires_in},
+            app.config["SECRET_KEY"],algorithm="HS256").decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token,app.config["SECRET_KEY"],
+                algorithms=["HS256"])["reset_password"]
+        except:
+            return
+        return User.query.get(id)
 
 
     def __repr__(self):
